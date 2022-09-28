9b1647f738bc3ef45e3cc100617fef950ff0f41d
Freddiiy <frederikgaller@live.dk>
2022-09-28 19:17:55 +0200

    ai

---
+++ my_exercises/08-1Exercise.py
@@ -0,0 +1,6 @@
+class Baby:
+    def __init__(self, weight_in_grams: float, age_in_months: int):
+        self.weight_in_grams = weight_in_grams
+        self.age_in_months = age_in_months
+
+def make_babies():
